76ac2310a7cfaba1dfbb374ee671d5da3648642f
Hiten Tandon <tandonhiten@gmail.com>
2023-12-08 20:19:30 +0530

    Feat: Made day 8 a bit faster

--- src/day8.rs
+++ src/day8.rs
@@ -1,11 +1,5 @@
-use num::Integer;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use std::collections::HashMap;
-
 use itertools::Itertools;
-
-const START: u32 = 0; //AAA => A => 0
-const END: u32 = 0b110011100111001; //ZZZ => Z => 25 => 11001 => D
+use num::Integer;
 
 fn encode(s: &str) -> u32 {
     s.bytes()
@@ -15,6 +9,8 @@ fn encode(s: &str) -> u32 {
 }
 
 pub fn get_path_length(input: &str) -> u64 {
+    const START: u32 = 0; //AAA => A => 0
+    const END: u32 = 0b110011100111001; //ZZZ => Z => 25 => 11001 => D
     let mut lines = input.lines();
     let instructions = lines
         .by_ref()
@@ -23,7 +19,9 @@ pub fn get_path_length(input: &str) -> u64 {
         .chars()
         .map(|x| if x == 'L' { 0_usize } else { 1_usize })
         .collect::<Vec<_>>();
-    let map: HashMap<u32, _> = lines
+    let mut map: [[u32; 2]; 32768] = [[0; 2]; 32768];
+
+    lines
         .skip(1)
         .filter_map(|line| line.split(" = ").collect_tuple())
         .map(|(src, dest)| {
@@ -34,17 +32,19 @@ pub fn get_path_length(input: &str) -> u64 {
 
             (src, [left, right])
         })
-        .collect();
+        .for_each(|(k, v)| map[k as usize] = v);
 
     let mut curr_node = START;
     let mut s_count = 0;
+
     for instruction in instructions.into_iter().cycle() {
         s_count += 1;
-        curr_node = map.get(&curr_node).unwrap()[instruction];
+        curr_node = map[curr_node as usize][instruction];
         if curr_node == END {
             return s_count;
         }
     }
+
     u64::max_value()
 }
 
@@ -57,9 +57,10 @@ pub fn get_ghost_path_length(input: &str) -> u64 {
         .unwrap()
         .chars()
         .map(|x| if x == 'L' { 0_usize } else { 1_usize })
-        .collect::<Vec<_>>();
+        .collect::<Box<[_]>>();
 
-    let map: HashMap<u32, _> = lines
+    let mut map: [[u32; 2]; 32768] = [[0; 2]; 32768];
+    lines
         .skip(1)
         .filter_map(|line| line.split(" = ").collect_tuple())
         .map(|(src, dest)| {
@@ -68,23 +69,24 @@ pub fn get_ghost_path_length(input: &str) -> u64 {
             let left = encode(&left[1..]);
             let right = encode(&right[..3]);
 
-            if src & 0b1_1111 == 0 {
+            if src & 31 == 0 {
                 curr_nodes.push(src);
             }
             (src, [left, right])
         })
-        .collect();
+        .for_each(|(k, v)| map[k as usize] = v);
 
     curr_nodes
-        .into_par_iter()
+        .into_iter()
         .map(|mut curr_node| {
             let mut s_count = 0;
             while curr_node & 31 != 25 {
                 s_count += 1;
                 curr_node =
-                    map.get(&curr_node).unwrap()[instructions[(s_count - 1) % instructions.len()]];
+                    map[curr_node as usize][instructions[(s_count - 1) % instructions.len()]];
             }
             s_count as u64
         })
-        .reduce(|| 1, |a, b| a.lcm(&b))
+        .reduce(|a, b| a.lcm(&b))
+        .unwrap()
 }
